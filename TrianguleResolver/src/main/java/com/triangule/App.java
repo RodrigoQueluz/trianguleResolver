d9eeae518d73f7fe707a269eaa808a18ff1d4f9d
RodrigoQueluz <rodrigoqluz@gmail.com>
2016-08-20 12:57:16 -0300

    First commit

---
+++ TrianguleResolver/src/main/java/com/triangule/App.java
@@ -0,0 +1,13 @@
+package com.triangule;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
